5089f7fc42079fe2b128f990fa555d72cb6fa9e6
James Chancey <w0014744@selu.edu>
2015-09-22 20:28:40 -0500

    initial commit

---
+++ src/main/java/com/overpaoered/robotremote/BtInterface.java
@@ -0,0 +1,154 @@
+package com.overpaoered.robotremote;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+public class BtInterface {
+
+	private BluetoothDevice device = null;
+	private BluetoothSocket socket = null;
+	private BluetoothAdapter mBluetoothAdapter = null;
+	private InputStream receiveStream = null;
+	private BufferedReader receiveReader = null;
+	private OutputStream sendStream = null;
+
+	private ReceiverThread receiverThread;
+
+	Handler handlerStatus, handlerMessage;
+	
+	public static int CONNECTED = 1;
+	public static int DISCONNECTED = 2;
+	static final String TAG = "Device";	
+
+	public BtInterface(Handler hstatus, Handler h) {
+		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+		handlerStatus = hstatus;
+		handlerMessage = h;		
+	}
+
+	public void connect() {
+		
+		Set<BluetoothDevice> setpairedDevices = mBluetoothAdapter.getBondedDevices();
+    	BluetoothDevice[] pairedDevices = (BluetoothDevice[]) setpairedDevices.toArray(new BluetoothDevice[setpairedDevices.size()]);
+	
+		boolean foundDevice = false;
+		for(int i=0;i<pairedDevices.length;i++) {
+			if(pairedDevices[i].getName().contains("HC-06")) {
+				device = pairedDevices[i];
+				try {
+					socket = device.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"));
+					receiveStream = socket.getInputStream();
+					receiveReader = new BufferedReader(new InputStreamReader(receiveStream));
+					sendStream = socket.getOutputStream();
+				} 
+				catch (IOException e) {
+					e.printStackTrace();
+				}
+				foundDevice = true;
+				break;
+			}
+		}
+		if(foundDevice == false){
+			Log.v(TAG, "You have not turned on your device");
+		}
+		
+		receiverThread = new ReceiverThread(handlerMessage);
+		new Thread() {
+			@Override public void run() {
+				try {
+					socket.connect();
+					
+					Message msg = handlerStatus.obtainMessage();
+					msg.arg1 = CONNECTED;
+	                handlerStatus.sendMessage(msg);
+	                
+					receiverThread.start();
+					
+				} 
+				catch (IOException e) {
+					Log.v("N", "Connection Failed : "+e.getMessage());
+					e.printStackTrace();
+				}
+			}
+		}.start();
+	}
+
+	public void close() {
+		try {
+			socket.close();
+			receiverThread.interrupt();
+			
+			Message msg = handlerStatus.obtainMessage();
+			msg.arg1 = DISCONNECTED;
+			handlerStatus.sendMessage(msg);
+            
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+		public void sendData(String data) {
+			try {
+				sendStream.write(data.getBytes());
+		        sendStream.flush();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	private class ReceiverThread extends Thread {
+		Handler handler;
+		
+		ReceiverThread(Handler h) {
+			handler = h;
+		}
+		
+		@Override public void run() {
+			while(socket != null) {
+				if (isInterrupted()){
+						try {
+							join();
+						}
+						catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+				}
+				try {
+					if(receiveStream.available() > 0) {
+						String dataToSend = "";
+						
+						dataToSend = receiveReader.readLine();
+						if (dataToSend != null){
+							Log.v(TAG, dataToSend);
+							Message msg = handler.obtainMessage();
+							Bundle b = new Bundle();
+							b.putString("receivedData", dataToSend);
+			                msg.setData(b);
+			                handler.sendMessage(msg);
+			                dataToSend = "";
+						}
+						
+					}
+				} 
+				catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+}
---
+++ src/main/java/com/overpaoered/robotremote/DroneRemoteActivity.java
@@ -0,0 +1,185 @@
+package com.overpaoered.robotremote;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DroneRemoteActivity extends Activity implements OnClickListener {
+	private TextView logview, readout;
+	private Button connect, deconnect, gpsMenu, incAlt, decAlt;
+	private ImageView forwardArrow, backArrow, rightArrow, leftArrow, stop, leftBack, rightBack, leftFor, rightFor;
+	private BluetoothAdapter mBluetoothAdapter = null;
+	
+	private String[] logArray = null;
+
+	private BtInterface bt = null;
+	
+	static final String TAG = "Device";
+	static final int REQUEST_ENABLE_BT = 3;
+
+	final Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+            String data = msg.getData().getString("receivedData");
+            addToLog(data);            
+        }
+    };
+
+    final Handler handlerStatus = new Handler() {
+        public void handleMessage(Message msg) {
+            int status = msg.arg1;
+            if(status == BtInterface.CONNECTED) {
+            	addToLog("Connected");
+            } else if(status == BtInterface.DISCONNECTED) {
+            	addToLog("Disconnected");
+            }
+        }
+    };
+
+    private void addToLog(String message){
+    	for (int i = 1; i < logArray.length; i++){
+        	logArray[i-1] = logArray[i];
+        }
+        logArray[logArray.length - 1] = message;
+        
+        logview.setText("");
+        for (int i = 0; i < logArray.length; i++){
+        	if (logArray[i] != null){
+        		logview.append(logArray[i] + "\n");
+        	}
+        }    	
+    }
+    
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ui);
+
+        
+        logview = (TextView)findViewById(R.id.logview);
+
+		readout = (TextView)findViewById(R.id.readOut);
+        readout.setText(R.string.manual);
+
+        logArray = new String[3];
+        
+        connect = (Button)findViewById(R.id.connect);
+        connect.setOnClickListener(this);
+        
+        deconnect = (Button)findViewById(R.id.deconnect);
+        deconnect.setOnClickListener(this);
+
+        gpsMenu = (Button)findViewById(R.id.gpsMenu);
+        gpsMenu.setOnClickListener(this);
+
+        incAlt = (Button)findViewById(R.id.increaseAlt);
+        incAlt.setOnClickListener(this);
+
+        decAlt = (Button)findViewById(R.id.decreaseAlt);
+        decAlt.setOnClickListener(this);
+        
+        forwardArrow = (ImageView)findViewById(R.id.forward);
+        forwardArrow.setOnClickListener(this);
+
+		leftFor = (ImageView)findViewById(R.id.leftFor);
+		leftFor.setOnClickListener(this);
+
+		rightFor = (ImageView)findViewById(R.id.rightFor);
+		rightFor.setOnClickListener(this);
+
+        backArrow = (ImageView)findViewById(R.id.back);
+        backArrow.setOnClickListener(this);
+
+		leftBack = (ImageView)findViewById(R.id.leftBack);
+		leftBack.setOnClickListener(this);
+
+		rightBack = (ImageView)findViewById(R.id.rightBack);
+		rightBack.setOnClickListener(this);
+
+        rightArrow = (ImageView)findViewById(R.id.right);
+        rightArrow.setOnClickListener(this);
+
+        leftArrow = (ImageView)findViewById(R.id.left);
+        leftArrow.setOnClickListener(this);
+
+        stop = (ImageView)findViewById(R.id.stop);
+        stop.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+        	Log.v(TAG, "Device does not support Bluetooth");
+        }
+		else{
+        	if (!mBluetoothAdapter.isEnabled()){
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        	}
+        	else{
+        		bt = new BtInterface(handlerStatus, handler);
+            }
+        }
+    }
+
+	protected void onActivityResult(int requestCode, int resultCode, Intent moreData){
+    	if (requestCode == REQUEST_ENABLE_BT){
+    		if (resultCode == Activity.RESULT_OK){
+    			bt = new BtInterface(handlerStatus, handler);
+    		}
+    		else if (resultCode == Activity.RESULT_CANCELED)
+    			Log.v(TAG, "BT not activated");
+    		else
+    			Log.v(TAG, "result code not known");
+    	}
+    	else{
+    		Log.v(TAG, "request code not known");    	
+    	}
+     }
+
+	@Override
+	public void onClick(View v) {
+		if(v == connect) {
+			addToLog("Trying to connect");
+			bt.connect();			
+		} 
+		else if(v == deconnect) {
+			addToLog("closing connection");
+			bt.close();			
+		}
+		else if(v == forwardArrow) {
+			addToLog("move forward");
+			bt.sendData("F");
+		}
+		else if(v == backArrow) {
+			addToLog("move back");
+			bt.sendData("B");
+		}
+		else if(v == rightArrow) {
+			addToLog("move right");
+			bt.sendData("R");
+		}
+		else if(v == leftArrow) {
+			addToLog("move left");
+			bt.sendData("L");
+		}
+		else if(v == stop) {
+			addToLog("stop");
+			bt.sendData("S");
+		}
+	}
+}
